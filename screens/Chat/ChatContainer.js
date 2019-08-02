8c1ef1ad895d10eec0a71c790aa46912e595bb68
khj6440 <gurwls7673@gmail.com>
2019-08-02 17:36:31 +0900

    #2.1 Add KeyboardAvoidingView

--- screens/Chat/ChatContainer.js
+++ screens/Chat/ChatContainer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ChatPresenter from "./ChatPresenter";
+import {StyleSheet} from "react-native";
 
 export default class ChatContainer extends React.Component {
+  state={
+    Focused:false
+  }
+  
   render() {
+    const {Focused} = this.state;
     return <ChatPresenter />;
   }
 }
--- screens/Chat/ChatPresenter.js
+++ screens/Chat/ChatPresenter.js
@@ -1,15 +1,51 @@
 import React from "react";
-import {Text} from "react-native";
+import { Text, KeyboardAvoidingView } from "react-native";
 import styled from "styled-components";
-import { BG_COLOR } from "../../constants/Colors";
+import { BG_COLOR, GREY_COLOR } from "../../constants/Colors";
+import Layout from "../../constants/Layout";
+import propType from "prop-types";
 
-const Container =styled.ScrollView`
-  flex:1;
-  background-color:${BG_COLOR};
-`; 
+const ChatContainer = styled.ScrollView`
+  background-color: yellow;
+`;
 
-const ChatPresenter=()=>
-<Container><Text>Chat</Text></Container>;
+const InputContainer = styled.View`
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
+`;
+const Input = styled.TextInput`
+  background-color: rgba(200, 200, 200, 2);
+  width: ${Layout.width / 1.6};
+  border-radius: 20px;
+  padding: 10px;
+`;
 
+const ChatPresenter = () => (
+  <KeyboardAvoidingView style={{ flex: 1 }} enabled behavior="padding">
+    <ChatContainer
+      contentContainerStyle={{
+        justifyContent: "flex-end",
+        flex: 1,
+        paddingVertical: 50
+      }}
+    >
+      <Text>abc</Text>
+
+      <InputContainer>
+        <Input
+          // onChangeText={handleSearchUpdate}
+          // value={searchTerm}
+          autoFocus={true}
+          placeholder="Search Movies and tv"
+          placeholderTextColor="black"
+          multiline={true}
+          // onSubmitEditing={onSubmitEditing}
+        />
+      </InputContainer>
+    </ChatContainer>
+  </KeyboardAvoidingView>
+);
+
+ChatPresenter.propType = {};
 
 export default ChatPresenter;
